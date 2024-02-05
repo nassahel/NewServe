79a19ce87b75f1af55d32964cff85877901556a5
Nassahel Elias <79935315+nassahel@users.noreply.github.com>
2024-02-05 20:56:54 -0300

    se arreglo nav movil

--- src/components/NavMovil.jsx
+++ src/components/NavMovil.jsx
@@ -22,19 +22,24 @@ const NavMovil = () => {
         <a href='#' className='text-2xl text-azulServe inline'><span className='text-rojoServe'>SERVE</span>MASTER</a>
       </div>
 
-      <div className={`${sideBar ? "w-full" : "w-0"} duration-300 fixed top-0 bottom-0 left-0 right-0 gap-4 bg-white flex flex-col overflow-hidden h-screen`}>
-        <button onClick={() => setSidebar(false)} className='block ml-auto mr-2 mt-2'><XMarkIcon className="h-8 w-8 text-[#08213f] hover:text-pink-500 duration-200" /></button>
-        <a onClick={() => setSidebar(false)} href='#' className='nav-mov'>HOME</a>
-        <a onClick={() => setSidebar(false)} href='#sectionTwo' className='nav-mov'>NOSOTROS</a>
-        <a onClick={() => setSidebar(false)} href='#sectionTree' className='nav-mov'>SERVICIOS</a>
-        <a onClick={() => setSidebar(false)} href='#sectionFour' className='nav-mov'>CONTÁCTO</a>
-        <div className='text-center mt-auto'>
-        <a href='#' className='text-2xl text-azulServe inline truncate'><span className='text-rojoServe'>SERVE</span>MASTER</a>
+      <div className={`${sideBar ? "w-full" : "w-0"} duration-300 fixed top-0 bottom-0 left-0 right-0 gap-4 bg-white flex justify-between flex-col overflow-hidden`}>
 
+        <div className='flex flex-col gap-6'>
+          <button onClick={() => setSidebar(false)} className='block ml-auto mr-2 mt-2'><XMarkIcon className="h-10 w-10 text-[#08213f] hover:text-pink-500 duration-200" /></button>
+          <a onClick={() => setSidebar(false)} href='#' className='nav-mov'>HOME</a>
+          <a onClick={() => setSidebar(false)} href='#sectionTwo' className='nav-mov'>NOSOTROS</a>
+          <a onClick={() => setSidebar(false)} href='#sectionTree' className='nav-mov'>SERVICIOS</a>
+          <a onClick={() => setSidebar(false)} href='#sectionFive' className='nav-mov'>CONTÁCTO</a>
         </div>
-        <div className='text-white bg-blue-950 text-center px-4 py-0.5 mt-auto truncate'>
-        Tel: 3865-607676 | 3865-252015
-      </div>
+        <div>
+          <div className='text-center mb-20'>
+            <a href='#' className='text-2xl text-azulServe inline truncate'><span className='text-rojoServe'>SERVE</span>MASTER</a>
+          </div>
+          <div className='text-white bg-blue-950 text-center px-4 py-0.5 truncate'>
+            Tel: 3865-607676 | 3865-252015
+          </div>
+        </div>
+
       </div>
     </motion.nav>
 
