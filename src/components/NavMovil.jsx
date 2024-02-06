5a3e3c05dab17ea39f0f6fbfa6bfb4502f7d866f
Nassahel Elias <79935315+nassahel@users.noreply.github.com>
2024-02-05 22:28:51 -0300

    se acelero la velocidad de nav celular

--- src/components/NavMovil.jsx
+++ src/components/NavMovil.jsx
@@ -22,7 +22,7 @@ const NavMovil = () => {
         <a href='#' className='text-2xl text-azulServe inline'><span className='text-rojoServe'>SERVE</span>MASTER</a>
       </div>
 
-      <div className={`${sideBar ? "w-full" : "w-0"} duration-300 fixed top-0 bottom-0 left-0 right-0 gap-4 bg-white flex justify-between flex-col overflow-hidden`}>
+      <div className={`${sideBar ? "w-full" : "w-0"} duration-200 fixed top-0 bottom-0 left-0 right-0 gap-4 bg-white flex justify-between flex-col overflow-hidden`}>
 
         <div className='flex flex-col gap-6'>
           <button onClick={() => setSidebar(false)} className='block ml-auto mr-2 mt-2'><XMarkIcon className="h-10 w-10 text-[#08213f] hover:text-pink-500 duration-200" /></button>
